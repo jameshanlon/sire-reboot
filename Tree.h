fac15c700c5721fc8bb95b78b3e8bf494461a594
James Hanlon <jameswhanlon@gmail.com>
2014-04-15 20:59:18 +0100

    Working on Tree structure.

--- Tree.h
+++ Tree.h
@@ -4,48 +4,37 @@
 #include <vector>
 #include <string>
 
-class Node;
+struct Node;
 
-class Tree {
-// The syntax tree base class
-public:
-  typedef enum {
-    t_SERVER, 
-    t_PROCESS, 
-    t_FUNCTION,
-    t_VAL, 
-    t_VAR,
-    t_ARRAY
-  } Type;
-
-  std::vector<Node> specification;
-  std::vector<Node> program;
+struct Tree {
+// The syntax tree base struct
+  std::vector<Node*> specification;
+  std::vector<Node*> program;
   void print();
 };
 
-class Node {
-// The tree node base class
+struct Node {
+// The tree node base struct
 public:
   Tree::Type type;
   Node(Tree::Type type) : type(type) {}
 };
 
-class Formal {
-};
-
-class Command {
-};
-
-class Specifier {
-};
-
-class Expr {
-};
+// Base Node types
+struct Specification;
+struct Command;
+struct Formal;
+struct Expr;
+struct Elem;
 
 // Program ====================================================================
 
-class Def : public Node {
-public:
+struct Def : public Node {
+  typedef enum {
+    t_SERVER, 
+    t_PROCESS, 
+    t_FUNCTION,
+  } Type;
   std::string name;
   std::vector<Formal*> args;
   std::vector<Command*> body;
@@ -61,33 +50,95 @@ public:
 
 // Specifications =============================================================
 
-// Declarations
-
-class Decl : public Node {
-public:
+struct Specification : public Node {
+  typedef enum {
+    VAR_DECL,
+    ARRAY_DECL,
+    VAL_ABBRV,
+    VAR_ABBRV,
+  } Type;
   std::string name;
-  Decl(Tree::Type type,
-       const std::string &name) :
-    Node(type),
+  Specification() {}
+}
+
+struct VarDecl : public Specification {
+  VarDecl(const std::string &name) :
+    Node(Specification::VAR_DECL),
     name(name) {}
 };
 
-class ArrayDecl : public Decl {
-public:
-  std::vector<Expr *> sizes;
+struct ArrayDecl : public Specification {
+  std::vector<Expr*> sizes;
   ArrayDecl(const std::string &name, 
-            const std::vector<Expr *> &sizes) :
-    Decl(Tree::t_ARRAY, name),
+            const std::vector<Expr*> &sizes) :
+    Specification(Specification::ARRAY_DECL),
     sizes(sizes) {}
 };
 
-// Abbreviations
-
-class Abbr : public Node {
-  //Specifier *spec;
+struct ValAbbrv : public Specification {
+  Elem *elem;
+  ValAbbrv(const std::string &name,
+           Elem *elem) :
+    Specification(Specification::VAL_ABBRV),
+    elem(elem) {}
 };
 
-class ValAbbr : public Abbr {
+struct VarAbbrv : public Specification {
+  Expr *expr;
+  ValAbbrv(const std::string &name,
+           Expr* expr) :
+    Specification(Specification::VAL_ABBRV),
+    expr(expr) {}
+};
+
+// Commands ===================================================================
+
+struct Cmd : public Node {
+  typedef enum {
+    SKIP,
+    STOP,
+    ASS,
+    IN,
+    OUT,
+    CONNECT,
+    ALT,
+    COND,
+    LOOP,
+    SEQ,
+    PAR
+  } Type;
+};
+
+// Formals ====================================================================
+
+struct Formal : public Node {
+  typedef enum {
+    VAR,
+    VAR_ARRAY,
+    CALL_ARRAY,
+    VAL
+  } Type;
+};
+
+// Expressions ================================================================
+
+struct Expr : public Node {
+  typedef enum {
+    UNARY,
+    BINARY
+    VALOF
+  } Type;
+};
+
+// Elements ===================================================================
+
+struct Elem : public Node {
+  typedef enum {
+    SUBSCRIPT,
+    FIELD,
+    NAME
+    LITERAL
+  } Type;
 };
 
 #endif
